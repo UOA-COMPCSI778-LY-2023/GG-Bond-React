09eeafd440647816407eeb0aa63d8d5bb071dbd3
it-tttt <1105974122@qq.com>
2023-12-01 17:01:41 +1300

    Initial version - with Google Maps

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Gmap from './components/Gmap/Gmap';
+
+function App() {
+  return (
+    <div className="App">
+      <Gmap></Gmap>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Gmap/Gmap.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import MapController from '../MapController/MapController';
+import Menuoptions from '../Menuoptions/Menuoptions';
+
+const containerStyle = {
+    width: '100%',
+    height: '100vh'
+};
+
+const center = {
+  lat: -36.842, 
+  lng: 174.760
+};
+
+
+function Gmap() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDwr4BTNTe-fH1AH0yOjuTfXxAItSANRXw"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    // const bounds = new window.google.maps.LatLngBounds(center);
+    // map.fitBounds(bounds);
+    const zoom = 17;
+    map.setZoom(zoom);
+    setMap(map);
+  }, []);
+  
+  
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={16}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { <Marker
+            position={{
+                lat: -36.842, 
+                lng: 174.760
+              }}
+        />}
+        <Menuoptions />
+        <MapController />
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+export default React.memo(Gmap)
