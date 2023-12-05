07527e79ff0eec795ee91bec1e2cf47fa209fa1a
1006413349 <65016202+1006413349@users.noreply.github.com>
2023-12-05 17:04:21 +1300

    added filter function

--- src/components/MenuOptions/MenuOptions.jsx
+++ src/components/MenuOptions/MenuOptions.jsx
@@ -4,24 +4,34 @@ import { IoIosSearch } from "react-icons/io";
 import { CiLocationArrow1 } from "react-icons/ci";
 import { LiaDrawPolygonSolid } from "react-icons/lia";
 import { FaLightbulb, FaRegLightbulb } from "react-icons/fa6";
+import { IoIosColorFilter } from "react-icons/io";
 import Tooltip from '../Tooltip/Tooltip';
 
 const MenuOptions = () => {
 
   const [dark, setDark] = useState(false);
+  const [isActive, setIsActive] = useState(false);
   const toggleDarkOrLightMode = () => {
     setDark(!dark);
+    setIsActive(!isActive);
+  };
+  const menuOptionsStyle = {
+    background: isActive ? 'white' : '#1c2330', // Change background color based on isActive
   };
 
+  const buttonStyle = {
+    color: isActive ? '#1c2330' : '#fff', // Change button color based on isActive
+  };
   return (
-    <div id="menuoptions">
-        <button className='menubutton'>
+    <div id="menuoptions" style={menuOptionsStyle}>
+        <button className='menubutton' style={buttonStyle}> 
         <span onClick={toggleDarkOrLightMode}>{dark ? <FaLightbulb /> : <FaRegLightbulb /> }</span>
           {/* <div>Toggle Dark Mode</div> */}
         </button>
-        <button><span><Tooltip text = "Draft"><LiaDrawPolygonSolid /></Tooltip></span></button> {/*Draft */}
-        <button><span><Tooltip text = "Localization"><CiLocationArrow1 /></Tooltip></span></button> {/*Localization*/}
-        <button><span><Tooltip text = "Search"><IoIosSearch /></Tooltip></span></button>  {/*Search*/}
+        <button style={buttonStyle}><span><Tooltip text = "Draft"><LiaDrawPolygonSolid /></Tooltip></span></button> {/*Draft */}
+        <button style={buttonStyle}><span><Tooltip text = "Localization"><CiLocationArrow1 /></Tooltip></span></button> {/*Localization*/}
+        <button style={buttonStyle}><span><Tooltip text = "Search"><IoIosSearch /></Tooltip></span></button>  {/*Search*/}
+        <button style={buttonStyle}><span><Tooltip text = "Filter"><IoIosColorFilter /></Tooltip></span></button>  {/*Search*/}
 
     </div>
   );
