2367e0c284f9bdf354c0a99ff77b01c1faa8a51e
1006413349 <65016202+1006413349@users.noreply.github.com>
2023-12-05 17:17:45 +1300

    updated filter function

--- src/components/MenuOptions/MenuOptions.jsx
+++ src/components/MenuOptions/MenuOptions.jsx
@@ -11,10 +11,29 @@ const MenuOptions = () => {
 
   const [dark, setDark] = useState(false);
   const [isActive, setIsActive] = useState(false);
+  const [showFilterDropdown, setShowFilterDropdown] = useState(false);
+  const [selectedFilters, setSelectedFilters] = useState([])
+
   const toggleDarkOrLightMode = () => {
     setDark(!dark);
     setIsActive(!isActive);
   };
+
+  const toggleFilterDropdown = () => {
+    setShowFilterDropdown(!showFilterDropdown);
+  };
+
+  const handleFilterSelect = (filterType) => {
+    setSelectedFilters(prevFilters => {
+      // Add or remove filter from the array
+      if (prevFilters.includes(filterType)) {
+        return prevFilters.filter(f => f !== filterType);
+      } else {
+        return [...prevFilters, filterType];
+      }
+    });
+  };
+
   const menuOptionsStyle = {
     background: isActive ? 'white' : '#1c2330', // Change background color based on isActive
   };
@@ -31,7 +50,19 @@ const MenuOptions = () => {
         <button style={buttonStyle}><span><Tooltip text = "Draft"><LiaDrawPolygonSolid /></Tooltip></span></button> {/*Draft */}
         <button style={buttonStyle}><span><Tooltip text = "Localization"><CiLocationArrow1 /></Tooltip></span></button> {/*Localization*/}
         <button style={buttonStyle}><span><Tooltip text = "Search"><IoIosSearch /></Tooltip></span></button>  {/*Search*/}
-        <button style={buttonStyle}><span><Tooltip text = "Filter"><IoIosColorFilter /></Tooltip></span></button>  {/*Search*/}
+
+        <button style={buttonStyle} onClick={toggleFilterDropdown}>
+        <span><Tooltip text="Filter"><IoIosColorFilter /></Tooltip></span>
+      </button>
+      {showFilterDropdown && (
+        <div className="filter-dropdown">
+          <div onClick={() => handleFilterSelect('cargo')}>Cargo</div>
+          <div onClick={() => handleFilterSelect('tanker')}>Tanker</div>
+          <div onClick={() => handleFilterSelect('passenger')}>Passenger</div>
+          {/* Add more ship types as needed */}
+        </div>
+      )}
+        
 
     </div>
   );
