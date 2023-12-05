7bab1f4e7cd2a2976055604c4e08970ae69f9076
1006413349 <65016202+1006413349@users.noreply.github.com>
2023-12-05 15:37:33 +1300

    mouse on events

--- src/components/Menuoptions/Menuoptions.jsx
+++ src/components/Menuoptions/Menuoptions.jsx
@@ -4,6 +4,7 @@ import { IoIosSearch } from "react-icons/io";
 import { CiLocationArrow1 } from "react-icons/ci";
 import { LiaDrawPolygonSolid } from "react-icons/lia";
 import { FaLightbulb, FaRegLightbulb } from "react-icons/fa6";
+import Tooltip from '../Tooltip/Tooltip';
 
 const MenuOptions = () => {
 
@@ -15,12 +16,12 @@ const MenuOptions = () => {
   return (
     <div id="menuoptions">
         <button className='menubutton'>
-          <span onClick={toggleDarkOrLightMode}>{dark ? <FaLightbulb /> : <FaRegLightbulb /> }</span>
+        <span onClick={toggleDarkOrLightMode}>{dark ? <FaLightbulb /> : <FaRegLightbulb /> }</span>
           {/* <div>Toggle Dark Mode</div> */}
         </button>
-        <button><span><LiaDrawPolygonSolid /></span></button> {/*Draft */}
-        <button><span><CiLocationArrow1 /></span></button> {/*Localization*/}
-        <button><span><IoIosSearch /></span></button>  {/*Search*/}
+        <button><span><Tooltip text = "Draft"><LiaDrawPolygonSolid /></Tooltip></span></button> {/*Draft */}
+        <button><span><Tooltip text = "Localization"><CiLocationArrow1 /></Tooltip></span></button> {/*Localization*/}
+        <button><span><Tooltip text = "Search"><IoIosSearch /></Tooltip></span></button>  {/*Search*/}
 
     </div>
   );
---
+++ src/components/Tooltip/Tooltip.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './Tooltip.css'; // Import your Tooltip CSS
+
+const Tooltip = ({ children, text }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  return (
+    <div className='tooltip-container' onMouseEnter={() => setShowTooltip(true)} onMouseLeave={() => setShowTooltip(false)}>
+      {children}
+      {showTooltip && <div className='tooltip-box'>{text}</div>}
+    </div>
+  );
+};
+
+export default Tooltip;
