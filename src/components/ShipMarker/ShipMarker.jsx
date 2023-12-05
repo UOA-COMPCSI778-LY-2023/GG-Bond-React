242f05ee3f5d35ceb90ed03a137ace1e7f13e2d2
it-tttt <41719927+it-tttt@users.noreply.github.com>
2023-12-05 21:29:54 +1300

    Update ShipMarker.jsx

--- src/components/ShipMarker/ShipMarker.jsx
+++ src/components/ShipMarker/ShipMarker.jsx
@@ -7,10 +7,28 @@ import ShipInfo from '../ShipInfo/ShipInfo';
 import shipMockData from '../ShipInfo/ShipMockData';
 
 
-const shipIcon = (heading) => L.divIcon({
-    className: 'custom-icon',
-    html: ReactDOMServer.renderToString(<MdOutlineNavigation style={{ color: 'green', transform: `rotate(${heading}deg)` }}/>),
-});
+const shipTypeDic = {
+    "Tank" : "red",
+    "Cargo" : "lightgreen",
+    "Fishing" : "burlywood",
+    "Craft" : "yellow",
+    "Pleasure" : "purple",
+    "Tugs" : "lightblue",
+    "Passenger" : "blue",
+    "Navigation Aids" : "pink",
+    "Unspecified" : "gray"
+}
+
+const shipIcon = (heading, type) => {
+    const color = shipTypeDic[type] || 'gray'; 
+  
+    return L.divIcon({
+      className: 'custom-icon',
+      html: ReactDOMServer.renderToString(
+        <MdOutlineNavigation style={{ color, transform: `rotate(${heading}deg)` }} />
+      ),
+    });
+  };
 
 const ShipMarker = ({ boatData }) => {
   const { name, type, speed, location, status } = boatData;
