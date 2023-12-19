26a049185fed975dce88f54d927fb0ff62c8affb
it-tttt <1105974122@qq.com>
2023-12-19 17:29:41 +1300

    add lat, lng and zoom level into path; remove useless files

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,12 +1,12 @@
 import L from "leaflet";
 import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { MapContainer, useMapEvents, ScaleControl } from "react-leaflet";
 import MenuOptions from "../MenuOptions/MenuOptions";
 import ShipInfo from "../ShipInfo/ShipInfo";
 import ShipMarker from "../ShipMarker/ShipMarker";
 import "leaflet/dist/leaflet.css";
-import SearchShip from "../SearchShip/SearchShip";
 import MapLayers from "../MapLayers/MapLayers";
 // import './Map.css'
 
@@ -19,6 +19,17 @@ const bounds = L.latLngBounds(corner1, corner2);
 function Map() {
     const [selectedBoat, setSelectedBoat] = useState();
     const [shipsBasicData, setShipsBasicData] = useState([]);
+
+    const navigate = useNavigate();
+    const params = useParams();
+    const { lat, lng, zoom } = params;
+    const latitude = parseFloat(lat) || center[0];
+    const longitude = parseFloat(lng) || center[1];
+    const zoomLevel = parseInt(zoom) || 2;
+
+    useEffect(() => {
+        navigate(`/${latitude}/${longitude}/${zoomLevel}`, { replace: true });
+    }, [latitude, longitude, zoomLevel, navigate]);
 
     const getShipBasicData = async (latLngNE, latLngSW) => {
         const type = "0";
@@ -46,17 +57,17 @@ function Map() {
 
     function GetMapDetail() {
         const map = useMapEvents({
-            zoomend: () => {
-                getShipBasicData(
-                    map.getBounds()._northEast,
-                    map.getBounds()._southWest
-                );
-            },
             moveend: () => {
+                const center = map.getCenter();
+                const zoom = map.getZoom();
+
                 getShipBasicData(
                     map.getBounds()._northEast,
                     map.getBounds()._southWest
                 );
+
+                const newUrl = `/${center.lat}/${center.lng}/${zoom}`;
+                navigate(newUrl, { replace: true });
             },
         });
 
@@ -93,8 +104,8 @@ function Map() {
         <div className="Map">
             <MapContainer
                 id="mapId"
-                center={center}
-                zoom={2}
+                center={[latitude, longitude]}
+                zoom={zoomLevel}
                 scrollWheelZoom={true}
                 maxBoundsViscosity={1.0}
                 maxBounds={bounds}
@@ -104,7 +115,6 @@ function Map() {
                 <ScaleControl position={"bottomleft"} />
                 <MapLayers />
                 <MenuOptions />
-                <SearchShip />
 
                 {shipsBasicData.map((boatData, index) => {
                     return (
