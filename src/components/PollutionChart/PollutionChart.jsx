f626d2bd4ab1c7561cf925d81d84872392c23ed6
xlen979 <505423939@qq.com>
2024-01-29 20:47:51 +1300

    use api for radar chart

--- src/components/PollutionChart/PollutionChart.jsx
+++ src/components/PollutionChart/PollutionChart.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Chart, registerables } from 'chart.js';
+import './PollutionChart.css'
+import RadarChart from './RadarChart';
 
 Chart.register(...registerables);
 
@@ -19,13 +21,13 @@ const ChartComponent = ({mmsi}) => {
                     '02.00', '04.00', '06.00', '08.00', '10.00','12.00', 
                     '14.00', '16.00', '18.00', '20.00', '22.00','24.00'],
                 datasets: [{
-                    label: 'co2 pollution',
+                    label: 'CO2 pollution',
                     data: fuelPollution,
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 1,
                     fill: false
                 }, {
-                    label: 'fuel consumption',
+                    label: 'Averge CO2 Consumption ',
                     data: fuelPollutionAvg,
                     borderColor: 'rgba(255, 99, 132, 1)',
                     borderWidth: 1,
@@ -35,8 +37,19 @@ const ChartComponent = ({mmsi}) => {
             options: {
                 scales: {
                     y: {
-                        min: 0,
+                        // min: 0,
                         // max: 100
+                    }
+                },
+                plugins: {
+                    legend: {
+                        labels: {
+                            font: {
+                                size: 12, 
+                                family: 'Arial',
+                                weight:'normal'
+                            }
+                        }
                     }
                 }
             }
@@ -51,13 +64,13 @@ const ChartComponent = ({mmsi}) => {
                 labels: ['Air Quality', 'Water Quality', 'Noise', 'Waste pollution', 'Power Consumption'],
                 datasets: [{
                     label: 'Environmental Impact 1',
-                    data: [65, 59, 90, 81, 56],
+                    data: [40, 88, 30, 40, 88],
                     backgroundColor: 'rgba(255, 99, 132, 0.2)',
                     borderColor: 'rgba(255, 99, 132, 1)',
                     borderWidth: 1
                 }, {
                     label: 'Environmental Impact 2',
-                    data: [28, 48, 40, 19, 96],
+                    data: [28, 48, 50, 55, 77],
                     backgroundColor: 'rgba(54, 162, 235, 0.2)',
                     borderColor: 'rgba(54, 162, 235, 1)',
                     borderWidth: 1
@@ -66,12 +79,21 @@ const ChartComponent = ({mmsi}) => {
             options: {
                 scales: {
                     r: {
-                        angleLines: {
-                            display: false
+                        // angleLines: {
+                        //     display: false
+                        // },
+                        // suggestedMin: 20, // 设置雷达图的最小范围
+                        // suggestedMax: 100, // 设置雷达图的最大范围
+                        // beginAtZero: true, // 使得雷达图的范围从0开始
+                        ticks: {
+                            callback: function(value, index) {
+                                // 这里可以根据index（轴的索引）来自定义每个轴的刻度显示
+                                // 示例：为每个轴设置不同的最大值
+                                const maxValues = [50, 100, 75, 60, 90];
+                                const maxValue = maxValues[index];
+                                return value <= maxValue ? value : null;
+                            }
                         },
-                        suggestedMin: 20, // 设置雷达图的最小范围
-                        suggestedMax: 100, // 设置雷达图的最大范围
-                        beginAtZero: true // 使得雷达图的范围从0开始
                     }
                 }
             }
@@ -110,8 +132,9 @@ const ChartComponent = ({mmsi}) => {
 
     return (
         <div>
-            <canvas ref={combinedChartRef} width="400" height="150"></canvas>
-            <canvas ref={radarChartRef} width="400" height="300"></canvas>
+            <canvas className="line-chart" ref={combinedChartRef} ></canvas>
+            {/* <canvas ref={radarChartRef} style={{marginTop:20}}></canvas> */}
+            <RadarChart mmsi={mmsi}></RadarChart>
         </div>
     );
 };
